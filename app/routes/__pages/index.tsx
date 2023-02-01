ef1c87d747236989a63403ddd2a0b569970023bc
goldenduck90 <goldenduck90919@gmail.com>
2023-02-01 18:15:39 +0700

    team

--- app/routes/__pages/index.tsx
+++ app/routes/__pages/index.tsx
@@ -12,6 +12,11 @@ import {
   LockClosedIcon,
 } from "@heroicons/react/24/outline";
 
+import sb from "~/assets/images/pages/sb.jpg";
+import ms from "~/assets/images/pages/ms.jpg";
+import bb from "~/assets/images/pages/bb.jpg";
+import ah from "~/assets/images/pages/ah.jpg";
+
 const features = [
   {
     name: "Lowest fees",
@@ -33,6 +38,33 @@ const features = [
     name: "Full Optionality",
     description: "Works with any chain, any wallet, any token, any exchange.",
     icon: FingerPrintIcon,
+  },
+];
+
+const people = [
+  {
+    name: "Sebastian Burkhard",
+    role: "Mega boss",
+    imageUrl: sb,
+    linkedinUrl: "https://www.linkedin.com/in/sebastian-b-4235233/",
+  },
+  {
+    name: "Markus Steffen",
+    role: "Legal & Compliance",
+    imageUrl: ms,
+    linkedinUrl: "https://www.linkedin.com/in/markus-steffen-b9131755/",
+  },
+  {
+    name: "Bryan Berry",
+    role: "Dev",
+    imageUrl: bb,
+    linkedinUrl: "https://www.linkedin.com/in/bryanwb/",
+  },
+  {
+    name: "Alice Huang",
+    role: "Growth & Community",
+    imageUrl: ah,
+    linkedinUrl: "https://www.linkedin.com/in/hecila/",
   },
 ];
 
@@ -130,6 +162,70 @@ export default function PagesMain() {
                   </div>
                 ))}
               </dl>
+            </div>
+          </div>
+        </div>
+        {/* PEOPLE */}
+
+        <div className="bg-gray-900">
+          <div className="mx-auto max-w-7xl py-12 px-6 lg:px-8 lg:py-24">
+            <div className="space-y-12">
+              <div className="space-y-5 text-center sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+                <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                  Team
+                </h2>
+                <p className="text-xl text-gray-300">
+                  Industry veterans in payments, crypto regulations, DeFi and
+                  Web3.
+                </p>
+              </div>
+              <ul
+                role="list"
+                className="grid grid-cols-2 sm:space-y-0 lg:grid-cols-4 lg:gap-2"
+              >
+                {people.map((person) => (
+                  <li
+                    key={person.name}
+                    className="rounded-lg py-10 px-6 text-center xl:px-10 xl:text-left"
+                  >
+                    <div className="space-y-4">
+                      <img
+                        className="mx-auto h-24 w-24 rounded-full "
+                        src={person.imageUrl}
+                        alt=""
+                      />
+                      <div className="">
+                        <div className="space-y-1 text-center text-lg font-medium leading-6">
+                          <h3 className="text-white">{person.name}</h3>
+                          <p className="text-indigo-400">{person.role}</p>
+                        </div>
+                      </div>
+                      <ul role="list" className="flex justify-center space-x-5">
+                        <li>
+                          <a
+                            href={person.linkedinUrl}
+                            className="text-gray-400 hover:text-gray-300"
+                          >
+                            <span className="sr-only">LinkedIn</span>
+                            <svg
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
