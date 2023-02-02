e7cd7370cacdbe9a250c73fcd6532302a3eab936
goldenduck90 <goldenduck90919@gmail.com>
2023-02-02 12:46:48 +0700

    privacy and tos

--- app/routes/__pages/privacy-policy.tsx
+++ app/routes/__pages/privacy-policy.tsx
@@ -1,6 +1,6 @@
 export default function PrivacyPolicyPage() {
   return (
-    <main className="-mt-24 h-screen pb-8">
+    <main className="-mt-24 pb-8">
       <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         {/* Main 3 column grid */}
         <div className="grid  grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
@@ -8,7 +8,152 @@ export default function PrivacyPolicyPage() {
           <div className="grid grid-cols-1 gap-4 lg:col-span-2">
             <section>
               <div className="overflow-hidden rounded-lg bg-slate-800 p-6 text-white shadow">
-                <h1>Privacy Policy</h1>
+                <h1 className="mt-8 pb-4 text-xl font-bold">Privacy Policy</h1>
+                <p>
+                  This Privacy Policy will explain how we use the personal data
+                  we collect from you when you use our website.
+                </p>
+                <h3 className="mt-4 font-bold">
+                  What data do we collect and how do we collect it?
+                </h3>
+                <p>
+                  We collect the following data when you contact us through the
+                  website: - Email Address
+                </p>
+                <h3 className="mt-4 font-bold">How will we use your data?</h3>
+                <p>
+                  We collect data so that we can contact and email you in order
+                  to respond to your inquires or – if you agree – to send you
+                  offers or other marketing related information.
+                </p>
+                <h3 className="mt-4 font-bold">How do we store your data?</h3>
+                <p>
+                  We securely store your data at our data center and will keep
+                  the information for ten years. Once this time period has
+                  expired, we will delete your data.
+                </p>
+                <h3 className="mt-4 font-bold">Use of our services</h3>
+                <p>
+                  We might collect additional information about you when you
+                  decide to use our services. In this case, our Privacy Policy
+                  applies to this information collected by us as well.{" "}
+                </p>
+                <h3 className="mt-4 font-bold">Marketing</h3>
+                <p>
+                  We would like to send you information about products and
+                  services of ours that we think you might like.
+                </p>
+                <p>
+                  If you have agreed to receive marketing, you may always opt
+                  out at a later date. You have the right at any time to stop us
+                  from contacting you for marketing purposes. If you no longer
+                  wish to be contacted for marketing purposes, you can
+                  unsubscribe from this service.{" "}
+                </p>
+                What are your data protection rights?
+                <p>
+                  We would like to make sure you are fully aware of all of your
+                  data protection rights. Every user is entitled to the
+                  following:
+                </p>
+                <p>
+                  The right to access - You have the right to request us for
+                  copies of your personal data. We may charge you a small fee
+                  for this service.
+                </p>
+                <p>
+                  The right to rectification - You have the right to request
+                  that we correct any information you believe is inaccurate. You
+                  also have the right to request us to complete information you
+                  believe is incomplete.
+                </p>
+                <p>
+                  The right to erasure - You have the right to request that we
+                  erase your personal data, under certain conditions.
+                </p>
+                <p>
+                  The right to restrict processing - You have the right to
+                  request that we restrict the processing of your personal data,
+                  under certain conditions.
+                </p>
+                <p>
+                  The right to object to processing - You have the right to
+                  object to us processing of your personal data, under certain
+                  conditions.
+                </p>
+                <p>
+                  The right to data portability - You have the right to request
+                  that we transfer the data that we have collected to another
+                  organization, or directly to you, under certain conditions.
+                </p>
+                <p>
+                  If you make a request, we have one month to respond to you. If
+                  you would like to exercise any of these rights, please contact
+                  us at our email:
+                </p>
+                <h3 className="mt-4 font-bold">What are cookies?</h3>
+                <p>
+                  Cookies are text files placed on your computer to collect
+                  standard Internet log information and visitor behaviour
+                  information. When you visit our websites, we may collect
+                  information from you automatically through cookies or similar
+                  technology.
+                </p>
+                <h3 className="mt-4 font-bold">How do we use cookies?</h3>
+                <p>
+                  We use cookies in a range of ways to improve your experience
+                  on our website, including: Understanding how you use our
+                  website.
+                </p>
+                <h3 className="mt-4 font-bold">
+                  What types of cookies do we use?
+                </h3>
+                <p>
+                  There are a number of different types of cookies, however, our
+                  website uses:
+                </p>
+                <p>
+                  Functionality – We use these cookies so that we recognize you
+                  on our website and remember your previously selected
+                  preferences. These could include what language you prefer and
+                  location you are in.
+                </p>
+                <p>
+                  Analysis / Performance. – These cookies allow us to recognise
+                  and count the number of visitors and to see how visitors move
+                  around our website when they are using it. This helps us to
+                  improve the way our website works, for example, by ensuring
+                  that users are finding what they are looking for easily.
+                </p>
+                <h3 className="mt-4 font-bold">How to manage cookies</h3>
+                <p>
+                  You can set your browser not to accept cookies. However, in a
+                  few cases, some of our website features may not function as a
+                  result.
+                </p>
+                <h3 className="mt-4 font-bold">
+                  Privacy policies of other websites
+                </h3>
+                <p>
+                  Our website may contain links to other websites. Our privacy
+                  policy applies only to our website, so if you click on a link
+                  to another website, you should read their privacy policy.
+                </p>
+                <h3 className="mt-4 font-bold">
+                  Changes to our privacy policy
+                </h3>
+                <p>
+                  We keep our privacy policy under regular review and place any
+                  updates on this website. This privacy policy was last updated
+                  on 15 November 2022.
+                </p>
+                How to contact us
+                <p>
+                  If you have any questions about our privacy policy, the data
+                  we hold on you, or you would like to exercise one of your data
+                  protection rights, please do not hesitate to contact us.
+                </p>
+                <p>Email us at: info@hiro.cash</p>
               </div>
             </section>
           </div>
--- app/routes/__pages/tos.tsx
+++ app/routes/__pages/tos.tsx
@@ -2,7 +2,7 @@ import { Content } from "../header";
 
 export default function TosPage() {
   return (
-    <main className="-mt-24 h-screen pb-8">
+    <main className="-mt-24 pb-8">
       <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         {/* Main 3 column grid */}
         <div className="grid  grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
@@ -10,7 +10,172 @@ export default function TosPage() {
           <div className="grid grid-cols-1 gap-4 lg:col-span-2">
             <section>
               <div className="overflow-hidden rounded-lg bg-slate-800 p-6 text-white shadow">
-                <h1>Terms Of Services</h1>
+                <h1 className="mt-8 pb-4 text-xl font-bold">Terms Of Use</h1>
+                <h2 className="mt-4 font-bold">Introduction </h2>
+
+                <p>
+                  This Website is owned by BitVit Corp. Panama. By using the
+                  Website, you agree to observe these Terms of Use.{" "}
+                </p>
+
+                <h3 className="mt-4 font-bold">Acceptable use</h3>
+
+                <p>
+                  You must comply with any instructions we give you about how to
+                  use the Website and must not do anything that interferes with
+                  or adversely affects the normal operation of the Website. You
+                  are responsible for ensuring the security of the systems and
+                  devices that you use to access the Website, including through
+                  use of appropriate virus-checking and other security software.{" "}
+                </p>
+
+                <h3 className="mt-4 font-bold">Intellectual property </h3>
+
+                <p>
+                  All of the intellectual property rights in and to the Website
+                  belong to us and our licensors. We may update and change the
+                  materials available on the Website, including by removing
+                  materials, at any time in our sole discretion without notice
+                  to you. You may download or print sections of the Website if
+                  needed for your own personal use, however, in other cases you
+                  may not reproduce any part of the Website without our express
+                  prior consent. To the extent that the Website contains
+                  functionality that allows you to access or download specific
+                  materials through the Website, your use of that functionality
+                  and the downloaded materials may be subject to separate terms
+                  and conditions which you will be asked to read and accept at
+                  the relevant time.{" "}
+                </p>
+
+                <h3 className="mt-4 font-bold">Website links </h3>
+
+                <p>
+                  The Website may have links or link to information that has not
+                  been devised, verified or tested by us or any of our officers,
+                  employees or agents. These are provided for your information
+                  only. We do not take any responsibility for that information,
+                  nor provide an endorsement as to its accuracy or completeness.
+                  We do not guarantee that the information, or the provision of
+                  the hyperlinks to you, does not infringe third party rights.
+                  If the Website contains a link to an external website, we do
+                  not endorse, recommend, approve, guarantee or introduce any
+                  third parties or the services and/or products that those third
+                  parties may offer. We accept no responsibility for them or any
+                  of the content available, or for any loss or damage that may
+                  arise from your use of them. You may provide a link to the
+                  home page of the Website at, on any website you operate, as
+                  long as you do not suggest any form of association, approval
+                  or endorsement on our part without our express prior
+                  permission and you promptly delete the link at our request.
+                  Except as set out in this clause, you may not link to the
+                  Website.{" "}
+                </p>
+
+                <h3 className="mt-4 font-bold">
+                  No financial services activities or advice
+                </h3>
+
+                <p>
+                  The information and any materials contained in this Website
+                  should not be considered as an offer or solicitation to buy or
+                  sell regulated instruments, provide advice, enable or take
+                  deposits or deliver any other regulated services of any kind
+                  in any jurisdiction, except as expressly stated and lawfully
+                  permitted. Any products or services described on this Website
+                  are not available to all persons and are subject to separate
+                  terms, conditions and restrictions. The information contained
+                  on the Website is not intended to provide and should not be
+                  construed as advice of any kind. You should obtain appropriate
+                  professional advice when necessary. It does not take into
+                  account your objectives, financial situation or needs.{" "}
+                </p>
+
+                <h3 className="mt-4 font-bold">Limitations of Liability </h3>
+
+                <p>
+                  While we will use reasonable endeavours, care and skill in
+                  operating the Website and the available services and products
+                  on it, we cannot promise that the Website and the products
+                  will always be available, meet your requirements or be
+                  completely free of faults, errors or compromise from
+                  cybersecurity events. To the maximum extent permitted by law,
+                  except as expressly set out in these Terms of Use, we exclude:{" "}
+                </p>
+
+                <p>
+                  all conditions, representations, warranties and statutory
+                  guarantees, whether express or implied, in relation to the
+                  Website and its products; and any liability, whether in
+                  contract, tort (including negligence), or otherwise, for any
+                  indirect or consequential loss, damage or expense incurred by
+                  you or any other user in connection with the Website and its
+                  products.{" "}
+                </p>
+
+                <p>
+                  To the extent we are unable by law to exclude any implied
+                  condition, representation, warranty or statutory guarantee in
+                  relation to the provision of goods or services through the
+                  Website, we limit our liability for a breach to the re-supply
+                  of those goods or services, or payment for such re-supply.
+                  This is without limitation to any other exclusions or
+                  restrictions of our or others’ liability in connection with
+                  this Website.{" "}
+                </p>
+
+                <h3 className="mt-4 font-bold">Reasonableness</h3>
+
+                <p>
+                  By using the Website, you agree that the exclusions of
+                  liability set out herein are reasonable. If you do not think
+                  they are reasonable, you must not use the Website.{" "}
+                </p>
+
+                <h3 className="mt-4 font-bold">Severability</h3>
+
+                <p>
+                  Should any provision of these Terms of Use be found to be
+                  unfair, invalid or unenforceable for any reason whatsoever,
+                  its unfairness, invalidity or unenforceability shall not
+                  affect the validity or enforceability of any other provision
+                  and any unfair, invalid or unenforceable provision shall be
+                  severable.{" "}
+                </p>
+
+                <p>
+                  The terms contained in these Terms if Use are severable so
+                  that if any part of them shall be declared void the remaining
+                  parts shall remain valid.{" "}
+                </p>
+
+                <h3 className="mt-4 font-bold">Entire agreement</h3>
+
+                <p>
+                  Other than certain specific functionalities of the Website,
+                  which may from time to time, be subject to separate,
+                  explicitly stated terms and conditions, these Terms of Use
+                  constitute the entire agreement between you and us in
+                  connection with your use of the Website and supersedes all
+                  previous agreements in respect of your use of the Website.{" "}
+                </p>
+
+                <h3 className="mt-4 font-bold">Privacy </h3>
+
+                <p>
+                  We will manage any personal information that we collect
+                  through the Website in accordance with our Privacy Policy.
+                </p>
+
+                <h3 className="mt-4 font-bold">
+                  Governing law and jurisdiction{" "}
+                </h3>
+
+                <p>
+                  These Terms of Use will be governed and construed in
+                  accordance with the laws of Panama and the user or visitor of
+                  this Website irrevocably submit to the exclusive jurisdiction
+                  of the courts of Panama.
+                </p>
               </div>
             </section>
           </div>
