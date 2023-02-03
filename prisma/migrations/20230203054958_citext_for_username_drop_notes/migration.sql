df6bb49c1c54bfb5bd4158c78278a9bc474878f6
goldenduck90 <goldenduck90919@gmail.com>
2023-02-03 12:50:31 +0700

    citext

---
+++ prisma/migrations/20230203054958_citext_for_username_drop_notes/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Note` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Password` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "citext";
+
+-- DropForeignKey
+ALTER TABLE "Note" DROP CONSTRAINT "Note_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Password" DROP CONSTRAINT "Password_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Account" ALTER COLUMN "username" SET DATA TYPE CITEXT;
+
+-- DropTable
+DROP TABLE "Note";
+
+-- DropTable
+DROP TABLE "Password";
+
+-- DropTable
+DROP TABLE "User";
