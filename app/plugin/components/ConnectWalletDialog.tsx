347e5062df5f772b894fb56ee1db73059c0e4ee4
goldenduck90 <goldenduck90919@gmail.com>
2023-02-13 14:23:22 +0700

    mobile link

--- app/plugin/components/ConnectWalletDialog.tsx
+++ app/plugin/components/ConnectWalletDialog.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useConnect } from "wagmi";
 
 import { connectorWalletIcon } from "../view/walletHelper";
@@ -12,47 +13,83 @@ export function ConnectWalletDialog() {
     error,
     pendingConnector,
   } = useConnect();
+  const [width, setWidth] = useState<number>(window.innerWidth);
 
-  return (
-    <>
+  const metaMask = connectors.find((c) => c.id === "metaMask");
+
+  const isMobile = width <= 768;
+  const walletPresent = window.ethereum != null;
+
+  if (isMobile) {
+    const urlToSelf = window.location.href.replace("https://", "");
+    const deepLinkUri = `https://metamask.app.link/dapp/${urlToSelf}`;
+    return (
       <div className="overflow-hidden bg-white pt-2">
         <ul className="divide-y divide-blue-300">
-          {connectors.map((connector) => (
-            <li
-              key={connector.id}
-              className="flex items-center px-0 py-3 hover:cursor-pointer hover:bg-blue-50 sm:px-6"
-            >
-              <div className="flex min-w-0 flex-1 items-center ">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src={connectorWalletIcon(connector)}
-                    alt=""
-                  />
-                </div>
-                <div className="min-w-0 flex-1 px-4">
-                  <button
-                    type="button"
-                    disabled={!connector.ready}
-                    onClick={() => connect({ connector })}
-                    className="w-full text-left text-lg font-bold text-indigo-600"
-                  >
-                    {connector.name}
-                    {(isLoading || isIdle) &&
-                      pendingConnector?.id === connector.id &&
-                      " (connecting)"}
-                  </button>
-                  {false &&
-                    isError &&
-                    pendingConnector?.id === connector.id && (
-                      <span>{error?.message}</span>
-                    )}
-                </div>
+          <li className="flex items-center px-0 py-3 hover:cursor-pointer hover:bg-blue-50 sm:px-6">
+            <div className="flex min-w-0 flex-1 items-center ">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src={connectorWalletIcon(metaMask)}
+                  alt=""
+                />
               </div>
-            </li>
-          ))}
+              <div className="min-w-0 flex-1 px-4">
+                <a
+                  href={deepLinkUri}
+                  className="w-full text-left text-lg font-bold text-indigo-600"
+                >
+                  Metamask
+                </a>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div className="overflow-hidden bg-white pt-2">
+          <ul className="divide-y divide-blue-300">
+            {connectors.map((connector) => (
+              <li
+                key={connector.id}
+                className="flex items-center px-0 py-3 hover:cursor-pointer hover:bg-blue-50 sm:px-6"
+              >
+                <div className="flex min-w-0 flex-1 items-center ">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src={connectorWalletIcon(connector)}
+                      alt=""
+                    />
+                  </div>
+                  <div className="min-w-0 flex-1 px-4">
+                    <button
+                      type="button"
+                      disabled={!connector.ready}
+                      onClick={() => connect({ connector })}
+                      className="w-full text-left text-lg font-bold text-indigo-600"
+                    >
+                      {connector.name}
+                      {(isLoading || isIdle) &&
+                        pendingConnector?.id === connector.id &&
+                        " (connecting)"}
+                    </button>
+                    {false &&
+                      isError &&
+                      pendingConnector?.id === connector.id && (
+                        <span>{error?.message}</span>
+                      )}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
