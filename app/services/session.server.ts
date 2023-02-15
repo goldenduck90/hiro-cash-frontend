8bec811597987e82a85a0c4ff12558c6f69d0fab
goldenduck90 <goldenduck90919@gmail.com>
2023-02-15 15:25:18 +0700

    use SESSION_SECRET from env var

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,6 +1,12 @@
 // app/services/session.server.ts
 import { createCookieSessionStorage } from "@remix-run/node";
 
+const SESSION_SECRET = process.env.SESSION_SECRET;
+
+if (!SESSION_SECRET) {
+  throw new Error("SESSION_SECRET must be provided");
+}
+
 // export the whole sessionStorage object
 export let sessionStorage = createCookieSessionStorage({
   cookie: {
@@ -8,7 +14,7 @@ export let sessionStorage = createCookieSessionStorage({
     sameSite: "lax", // this helps with CSRF
     path: "/", // remember to add this so the cookie will work in all routes
     httpOnly: true, // for security reasons, make this cookie http only
-    secrets: ["8t46BmkI6t8hpR/+7jv2VQdP2T7OcubTMm1c0I74ZE8="], // replace this with an actual secret
+    secrets: [SESSION_SECRET], // replace this with an actual secret
     secure: process.env.NODE_ENV === "production", // enable this in prod only
   },
 });
