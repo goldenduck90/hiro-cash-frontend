9c609d4d1864131c0bfadf979812d8ef08979c96
goldenduck90 <goldenduck90919@gmail.com>
2023-02-25 10:06:40 +0700

    fix layout padding

--- app/routes/__app/home.tsx
+++ app/routes/__app/home.tsx
@@ -20,7 +20,7 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function HomePage() {
   return (
-    <div className="flex h-full flex-col bg-slate-800 p-6">
+    <div className="flex h-full flex-col bg-slate-800">
       <Outlet />
     </div>
   );
